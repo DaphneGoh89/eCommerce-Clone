8955a516bfe7afe0bf8079822dcb08eb78eae3b3
DaphneGoh89 <115914140+DaphneGoh89@users.noreply.github.com>
2023-03-03 16:50:35 +0800

    Added ProductCart component

--- ecclone-app/src/components/Cart/ProductCart.jsx
+++ ecclone-app/src/components/Cart/ProductCart.jsx
@@ -1,7 +1,50 @@
 import React from "react";
 
+function getImageUrl(name) {
+  return new URL(`../../assets/images/${name}.webp`, import.meta.url).href;
+}
+
 const ProductCart = () => {
-  return <div>ProductCart</div>;
+  return (
+    <>
+      <div className="text-xxs flex flex-row space-x-3 pt-4 border-t-[1px] border-fontDarkGrey/30">
+        <img
+          src={getImageUrl("hy5532-014_msousv0o9urzhxyh")}
+          className="w-28 aspect-auto"
+        ></img>
+
+        <div className="flex-grow flex flex-row justify-between items-start space-x-3">
+          <div className="flex-grow md:flex md:flex-row md:justify-between">
+            <div className="md:flex-grow sm:w-full">
+              <p>Shirlene High Waist Shorts</p>
+              <p className="mb-2">White / S</p>
+            </div>
+            <div className="md:flex md:flex-row md:space-x-3">
+              <p className="mb-2 md:py-1.5">S$39.00</p>
+              <select
+                id="quantity"
+                name="quantity"
+                data-te-select-init
+                className="border-[1px] border-fontExtraLightGrey/50 bg-transparent w-14 sm:h-fit py-0.5 rounded-sm text-center focus:outline-none"
+              >
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+              </select>
+            </div>
+          </div>
+
+          <p className="md:py-1">S$ 78.00</p>
+        </div>
+      </div>
+      <div className="text-xxxs flex flex-row justify-end space-x-3 my-2">
+        <p>Edit</p>
+        <p>Remove</p>
+      </div>
+    </>
+  );
 };
 
 export default ProductCart;
--- ecclone-app/src/components/Pages/Cart.jsx
+++ ecclone-app/src/components/Pages/Cart.jsx
@@ -18,142 +18,148 @@ const Cart = () => {
         My Shopping Bag (2)
       </div>
       <Link to="/shop">
-        <p className="font-poppins text-xxs text-linkTealGreen underline mt-4 mb-2 hover:no-underline">
+        <p className="font-poppins text-xxs text-linkTealGreen underline mt-4 mb-3 hover:no-underline">
           Continue shopping
         </p>
       </Link>
-      {/* CART ITEMS */}
-      <ProductCart />
+      <div className="grid grid-cols-1 md:grid-cols-3 md:gap-5">
+        {/* CART ITEMS */}
 
-      {/* ORDER SUMMARY */}
-      <div
-        className={`border-1px w-full font-poppins text-xxs bg-white py-4 px-6`}
-      >
-        <p className="text-xs font-semibold tracking-widest mb-3">
-          ORDER SUMMARY
-        </p>
-        {/* Subtotal */}
-        <div className="flex flex-row justify-between mb-1.5">
-          <p>Subtotal</p>
-          <p>S$ 104.90</p>
+        <div className="col-span-2">
+          <ProductCart />
+          <ProductCart />
+          <ProductCart />
         </div>
-        {/* Shipping */}
-        <div className="flex flex-row justify-between mb-1.5">
-          <p>Shipping & Handling</p>
-          <p>S$ 0.00</p>
-        </div>
-        {/* GST Accordion*/}
-        <div className="flex flex-row justify-between mb-1.5 border-t-[1px] pt-1.5">
-          <p>GST (Included)</p>
-          <span
-            className={`ml-auto h-4 w-4 shrink-0 transition-transform duration-200 ease-in-out motion-reduce:transition-none ${
-              showGst ? "rotate-180" : ""
-            }`}
-            onClick={() => {
-              setShowGst(!showGst);
-            }}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1"
-              stroke="currentColor"
-              className="h-4 w-4"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                stroke="black"
-                d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-              />
-            </svg>
-          </span>
-        </div>
-        {/* GST Calculation */}
+        {/* ORDER SUMMARY */}
         <div
-          className={`flex flex-row justify-between mb-1.5 pt-1.5 ${
-            showGst ? "block" : "hidden"
-          }`}
+          className={`border-[1px] w-full font-poppins text-xxs bg-white py-4 px-6`}
         >
-          <p>GST (8%) (Included)</p>
-          <p>S$ 7.77</p>
-        </div>
-        {/* Promo Code Accordion */}
-        <div className="flex flex-row justify-between mb-1.5 border-t-[1px] pt-1.5">
-          <p
-            className={`${
-              showPromo ? "text-black" : "text-fontExtraLightGrey"
+          <p className="text-xs font-semibold tracking-widest mb-3">
+            ORDER SUMMARY
+          </p>
+          {/* Subtotal */}
+          <div className="flex flex-row justify-between mb-1.5">
+            <p>Subtotal</p>
+            <p>S$ 104.90</p>
+          </div>
+          {/* Shipping */}
+          <div className="flex flex-row justify-between mb-1.5">
+            <p>Shipping & Handling</p>
+            <p>S$ 0.00</p>
+          </div>
+          {/* GST Accordion*/}
+          <div className="flex flex-row justify-between mb-1.5 border-t-[1px] pt-1.5">
+            <p>GST (Included)</p>
+            <span
+              className={`ml-auto h-4 w-4 shrink-0 transition-transform duration-200 ease-in-out motion-reduce:transition-none ${
+                showGst ? "rotate-180" : ""
+              }`}
+              onClick={() => {
+                setShowGst(!showGst);
+              }}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1"
+                stroke="currentColor"
+                className="h-4 w-4"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  stroke="black"
+                  d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+                />
+              </svg>
+            </span>
+          </div>
+          {/* GST Calculation */}
+          <div
+            className={`flex flex-row justify-between mb-1.5 pt-1.5 ${
+              showGst ? "block" : "hidden"
             }`}
           >
-            Promo Code
-          </p>
-          <p
-            className="text-sm cursor-pointer"
-            onClick={() => setShowPromo(!showPromo)}
-          >
-            {showPromo ? <HiMinusSm /> : <HiPlusSm />}
-          </p>
-        </div>
-        {/* Promo Code Input */}
-        <div className={`${showPromo ? "block mb-2" : "hidden"}`}>
-          <div className="flex flex-row justify-between items-center space-x-20 mb-1 pt-1.5">
-            <input
-              type="text"
-              id="promoCode"
-              name="promoCode"
-              className="flex-grow text-xxxs p-2 border-[1px] border-fontDarkGrey rounded-sm"
-              placeholder="Enter promo code here"
-            ></input>
-            <button className="text-fontExtraLightGrey hover:text-fontDarkGrey border-[1px] border-fontDarkGrey font-semibold py-2 px-6 rounded-sm text-xxs md:text-xs">
-              APPLY
-            </button>
+            <p>GST (8%) (Included)</p>
+            <p>S$ 7.77</p>
           </div>
-          <p className="text-xxxs text-fontExtraLightGrey">
-            Only one promo code can be applied per transaction.
-          </p>
-        </div>
-        {/* Gift Card Accordion */}
-        <div className="flex flex-row justify-between mb-1.5 border-t-[1px] pt-1.5">
-          <p
-            className={`${
-              showGiftCard ? "text-black" : "text-fontExtraLightGrey"
-            }`}
-          >
-            Gift Card & Welcome Offer
-          </p>
-          <p
-            className="text-sm cursor-pointer"
-            onClick={() => setShowGiftCard(!showGiftCard)}
-          >
-            {showGiftCard ? <HiMinusSm /> : <HiPlusSm />}
-          </p>
-        </div>
-        {/* Gift Card Input */}
-        <div className={`${showGiftCard ? "block mb-2" : "hidden"}`}>
-          <div className="flex flex-row justify-between items-center space-x-20 mb-1 pt-1.5">
-            <input
-              type="text"
-              id="promoCode"
-              name="promoCode"
-              className="flex-grow text-xxxs p-2 border-[1px] border-fontDarkGrey rounded-sm"
-              placeholder="Enter gift code here"
-            ></input>
-            <button className="text-fontExtraLightGrey hover:text-fontDarkGrey border-[1px] border-fontDarkGrey font-semibold py-2 px-6 rounded-sm text-xxs md:text-xs">
-              APPLY
-            </button>
+          {/* Promo Code Accordion */}
+          <div className="flex flex-row justify-between mb-1.5 border-t-[1px] pt-1.5">
+            <p
+              className={`${
+                showPromo ? "text-black" : "text-fontExtraLightGrey"
+              }`}
+            >
+              Promo Code
+            </p>
+            <p
+              className="text-sm cursor-pointer"
+              onClick={() => setShowPromo(!showPromo)}
+            >
+              {showPromo ? <HiMinusSm /> : <HiPlusSm />}
+            </p>
           </div>
-          <p className="text-xxxs text-fontExtraLightGrey">
-            Multiple gift cards can be applied per transaction.
-          </p>
+          {/* Promo Code Input */}
+          <div className={`${showPromo ? "block mb-2" : "hidden"}`}>
+            <div className="flex flex-row justify-between items-center space-x-20 mb-1 pt-1.5">
+              <input
+                type="text"
+                id="promoCode"
+                name="promoCode"
+                className="flex-grow text-xxxs p-2 border-[1px] border-fontDarkGrey rounded-sm"
+                placeholder="Enter promo code here"
+              ></input>
+              <button className="text-fontExtraLightGrey hover:text-fontDarkGrey border-[1px] border-fontDarkGrey font-semibold py-2 px-6 md:font-normal md:px-3 rounded-sm text-xxs md:text-xxs">
+                APPLY
+              </button>
+            </div>
+            <p className="text-xxxs text-fontExtraLightGrey">
+              Only one promo code can be applied per transaction.
+            </p>
+          </div>
+          {/* Gift Card Accordion */}
+          <div className="flex flex-row justify-between mb-1.5 border-t-[1px] pt-1.5">
+            <p
+              className={`${
+                showGiftCard ? "text-black" : "text-fontExtraLightGrey"
+              }`}
+            >
+              Gift Card & Welcome Offer
+            </p>
+            <p
+              className="text-sm cursor-pointer"
+              onClick={() => setShowGiftCard(!showGiftCard)}
+            >
+              {showGiftCard ? <HiMinusSm /> : <HiPlusSm />}
+            </p>
+          </div>
+          {/* Gift Card Input */}
+          <div className={`${showGiftCard ? "block mb-2" : "hidden"}`}>
+            <div className="flex flex-row justify-between items-center space-x-20 mb-1 pt-1.5">
+              <input
+                type="text"
+                id="promoCode"
+                name="promoCode"
+                className="flex-grow text-xxxs p-2 border-[1px] border-fontDarkGrey rounded-sm"
+                placeholder="Enter gift code here"
+              ></input>
+              <button className="text-fontExtraLightGrey hover:text-fontDarkGrey border-[1px] border-fontDarkGrey font-semibold py-2 px-6 md:font-normal md:px-3 rounded-sm md:text-xxs text-xxs">
+                APPLY
+              </button>
+            </div>
+            <p className="text-xxxs text-fontExtraLightGrey">
+              Multiple gift cards can be applied per transaction.
+            </p>
+          </div>
+          {/* Calculate Order Total */}
+          <div className="text-xs font-bold mb-7 pt-1.5 border-t-[1px] border-fontDarkGrey flex flex-row justify-between">
+            <p>Total</p>
+            <p>S$ 104.90</p>
+          </div>
+          {/* Checkout Button */}
+          <ButtonSubmit btnText="CHECKOUT" handleClick={handleClick} />
         </div>
-        {/* Calculate Order Total */}
-        <div className="text-xs font-bold mb-7 pt-1.5 border-t-[1px] border-fontDarkGrey flex flex-row justify-between">
-          <p>Total</p>
-          <p>S$ 104.90</p>
-        </div>
-        {/* Checkout Button */}
-        <ButtonSubmit btnText="CHECKOUT" handleClick={handleClick} />
       </div>
     </div>
   );
